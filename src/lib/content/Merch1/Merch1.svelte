f0be2c687a80e2d0cf8ca13895547a43edb9f1e0
Brandon Shields <braven112@gmail.com>
2023-10-07 01:57:27 -0700

    feat: variant 1

---
+++ src/lib/content/Merch1/Merch1.svelte
@@ -0,0 +1,28 @@
+<script>
+  import "./Merch1.css";
+  import uplift from '$lib/images/uplift.svg';
+  import lines from '$lib/images/lines.svg';
+</script>
+<article class="merch" data-variant="1">
+  <picture class="merch__img">
+    <img class="imgholder" src={uplift} alt="uplift" />
+  </picture>
+  <div class="merch__content">
+      <p class="product__title-sm" data-type="headline-sm">Book Now. Pay Later.</p>
+      <p class="product__title-md" data-type="headline-md">Book Now. Pay Later.</p>
+      <p class="product__title-lg" data-type="headline-lg">Book Now. Pay Later.</p>
+      <p class="product__bodycopy" data-type="bodycopy">Choose Uplift, a flexible way to pay for what matters most.</p> 
+            
+      <auro-hyperlink class="product__cta-sm" data-type="standard" ondark type="nav" href="https://www.alaskaair.com">Book Now ></auro-hyperlink>
+      <auro-hyperlink class="product__cta-md" type="cta" href="https://www.alaskaair.com">Book Now</auro-hyperlink>
+      <p class="product__fineprint-xs fineprint">Restrictions apply</p>
+      <p class="product__fineprint-sm fineprint">Terms and conditions apply. Must apply here. Offers vary elsewhere.</p>
+      <p class="product__fineprint-lg fineprint">Terms and conditions apply. Must apply here. Offers vary elsewhere. Offer void outside of the United States.  Must complete applicaton in English.</p>
+  </div>
+</article>
+
+
+<style>
+
+</style>
+      
