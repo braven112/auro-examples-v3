56488be7aec6bf41f63c5b7d7a22444e317bdca7
Brandon Shields <braven112@gmail.com>
2023-10-10 09:11:08 -0700

    cleanup html

--- src/lib/content/Merch3/Merch3Award.svelte
+++ src/lib/content/Merch3/Merch3Award.svelte
@@ -20,16 +20,13 @@
   </picture>
   <div class="merch__content">
       <!--Headlines-->  
-      <p class="product__title-sm" data-type="headline-sm">{campaign.headlineSm}</p>
       <p class="product__title-md" data-type="headline-md">{campaign.headlineMd}</p>
-      <p class="product__title-lg" data-type="headline-lg">{campaign.headlineLg}</p>
       <!--Body Copy-->
-      <p class="product__bodycopy" data-type="bodycopy">{campaign.bodyCopyLg}</p> 
+      <p class="product__bodycopy" data-type="bodycopy">{campaign.bodyCopyMd}</p> 
       <p class="product__flight-details auro_heading auro_heading--300" data-type="flight-details">{flight.origin} <sup class="airport-code">{flight.originCode}</sup> <auro-icon category="interface" name="arrow-right"></auro-icon> {flight.destination} <sup class="airport-code">{flight.destinationCode}</sup></p> 
       <p class="product__flight-date" data-type="flight-date"><auro-icon category="interface" name="calendar"></auro-icon> Monday, April 1st</p> 
       <!--CTA-->
-      <auro-hyperlink class="product__cta-sm" data-type="standard" type="nav" href="{campaign.ctaLinkLg}?{campaign.campaignId}">{campaign.ctaSm}</auro-hyperlink>
-      <auro-hyperlink class="product__cta-md" type="cta" href="{campaign.ctaLinkMd}">{campaign.ctaMd}?{campaign.campaignId}</auro-hyperlink>
+      <auro-hyperlink class="product__cta-md" data-type="standard" type="nav" href="{campaign.ctaLinkMd}?{campaign.campaignId}">{campaign.ctaMd}</auro-hyperlink>
       <!--Disclaimer-->
       <p class="product__fineprint-sm fineprint">{campaign.fineprintSm}</p>
       <p class="product__fineprint-md fineprint">{campaign.fineprintMd}</p>
