54b824bcb334d44aff01866c4fb8f85474598355
Brandon Shields <braven112@gmail.com>
2023-10-09 20:05:05 -0700

    feat: read content from json

--- src/lib/content/Merch3/Merch3Deal.svelte
+++ src/lib/content/Merch3/Merch3Deal.svelte
@@ -2,40 +2,42 @@
   import "./Merch3.css";
   import planes from '$lib/images/planes.svg';
   import lines from '$lib/images/lines.svg';
+  import campaign from './Campaign_Data3.json';
+  import flight from './Flight_Data3.json';
 </script>
 <article class="merch" data-variant="3">
   <picture class="merch__img">
-    <!-- <img class="imgholder" src={card} alt="Card" /> -->
     <div class="merch__graphics">
-      <auro-avatar type="md" code="anc"></auro-avatar>
+      <auro-avatar type="md" code="{flight.destinationCode}"></auro-avatar>
       <div class="product__offer-wrapper">
-        <div class="product__offer">$49</div>
-        <div class="product__offer-detail">one way</div>
+        <!-- Offer -->
+        <div class="product__offer">{flight.offer}</div>
+        <div class="product__offer-detail">{flight.offerDetail}</div>
       </div>
+      <!--Design Element-->
       <img class="product__graphic-detail" src={planes} alt="planes" />
     </div>
   </picture>
   <div class="merch__content">
-      <p class="product__title-sm" data-type="headline-sm">Hey Anita</p>
-      <p class="product__title-md" data-type="headline-md">Hey Anita</p>
-      <p class="product__title-lg" data-type="headline-lg">Hey Anita</p>
-      <p class="product__bodycopy" data-type="bodycopy">A flight you just searched is as low as $49 one way</p> 
-      <p class="product__flight-details auro_heading auro_heading--300" data-type="flight-details">Seattle, WA <sup class="airport-code">Sea</sup> <auro-icon category="interface" name="arrow-right"></auro-icon> Anchorage, AK <sup class="airport-code">ANC</sup></p> 
+      <!--Headlines-->  
+      <p class="product__title-sm" data-type="headline-sm">{campaign.headlineSm}</p>
+      <p class="product__title-md" data-type="headline-md">{campaign.headlineMd}</p>
+      <p class="product__title-lg" data-type="headline-lg">{campaign.headlineLg}</p>
+      <!--Body Copy-->
+      <p class="product__bodycopy" data-type="bodycopy">{campaign.bodyCopyLg}</p> 
+      <p class="product__flight-details auro_heading auro_heading--300" data-type="flight-details">{flight.origin} <sup class="airport-code">{flight.originCode}</sup> <auro-icon category="interface" name="arrow-right"></auro-icon> {flight.destination} <sup class="airport-code">{flight.destinationCode}</sup></p> 
       <p class="product__flight-date" data-type="flight-date"><auro-icon category="interface" name="calendar"></auro-icon> Monday, April 1st</p> 
-      
-      <auro-hyperlink class="product__cta-sm" data-type="standard" type="nav" href="https://www.alaskaair.com">Book Now ></auro-hyperlink>
-      <auro-hyperlink class="product__cta-md" type="cta" href="https://www.alaskaair.com">Book Now</auro-hyperlink>
-      <p class="product__fineprint-xs fineprint">Restrictions apply</p>
-      <p class="product__fineprint-sm fineprint">Terms and conditions apply. Must apply here. Offers vary elsewhere.</p>
-      <p class="product__fineprint-lg fineprint">Terms and conditions apply. Must apply here. Offers vary elsewhere. Offer void outside of the United States.  Must complete applicaton in English.</p>
+      <!--CTA-->
+      <auro-hyperlink class="product__cta-sm" data-type="standard" type="nav" href="{campaign.ctaLinkLg}?{campaign.campaignId}">{campaign.ctaSm}</auro-hyperlink>
+      <auro-hyperlink class="product__cta-md" type="cta" href="{campaign.ctaLinkMd}">{campaign.ctaMd}?{campaign.campaignId}</auro-hyperlink>
+      <!--Disclaimer-->
+      <p class="product__fineprint-sm fineprint">{campaign.fineprintSm}</p>
+      <p class="product__fineprint-md fineprint">{campaign.fineprintMd}</p>
+      <p class="product__fineprint-lg fineprint">{campaign.fineprintLg}</p>
+      <!--Design Element-->
       <div class="product__graphic-wrapper">
         <img class="product__graphic-lines" src={lines} alt="lines" />
       </div>
   </div>
 </article>
-
-
-<style>
-
-</style>
       
