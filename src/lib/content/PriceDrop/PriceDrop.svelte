4c0ee6510c24c61ad9430c0b2f6460be930830b3
Marnel Mangrubang <marnel.mangrubang@alaskaair.com>
2023-10-19 17:10:33 -0700

    pricedrop playground

---
+++ src/lib/content/PriceDrop/PriceDrop.svelte
@@ -0,0 +1,49 @@
+<script>
+  import "./PriceDrop.css";
+  import planes from '$lib/images/planes.svg';
+  import lines from '$lib/images/lines.svg';
+  import pricedrop from './PriceDrop_Data.json';
+  // import flight from './PriceDrop_Data.json';
+</script>
+<div class="containment">
+  <article class="merch" data-variant="3">
+    <picture class="merch__img">
+      <div class="merch__graphics">
+        <auro-avatar type="md" code="{pricedrop.pricedrop_destination}"></auro-avatar>
+        <div class="product__offer-wrapper">
+          <!-- Offer -->
+          <div class="product__offer">{pricedrop.pricedrop_pricedrop_percentage}</div>
+          <!-- <div class="product__offer-detail">{pricedrop.pricedrop_flight_type}</div> -->
+        </div>
+        <!--Design Element-->
+        <img class="product__graphic-detail" src={planes} alt="planes" />
+      </div>
+    </picture>
+    <div class="merch__content">
+        <!--Headlines-->  
+        <p class="product__title-md" data-type="headline-md">{pricedrop.headline_small} {pricedrop.pricedrop_first_name}</p>
+        <!--Body Copy-->
+        <p class="product__bodycopy" data-type="bodycopy">{pricedrop.body_copy_medium}</p> 
+        <p class="product__flight-details auro_heading auro_heading--300" data-type="flight-details">
+          {pricedrop.pricedrop_origin_city} 
+          <sup class="airport-code">{pricedrop.pricedrop_origin}</sup> 
+          <auro-icon category="interface" name="arrow-right"></auro-icon> 
+          {pricedrop.pricedrop_destination_city} 
+          <sup class="airport-code">{pricedrop.pricedrop_destination}</sup>
+        </p> 
+        <p class="product__flight-date" data-type="flight-date"><auro-icon category="interface" name="calendar"></auro-icon> 
+          {pricedrop.pricedrop_departure_date}
+        </p> 
+        <!--CTA-->
+        <auro-hyperlink class="product__cta-md" data-type="standard" type="nav" href="{pricedrop.cta_medium}?{pricedrop.campaign_id}">{pricedrop.cta_medium}</auro-hyperlink>
+        <!--Disclaimer-->
+        <p class="product__fineprint-sm fineprint">{pricedrop.fineprint_small}</p>
+        <p class="product__fineprint-md fineprint">{pricedrop.fineprint_medium}</p>
+        <p class="product__fineprint-lg fineprint">{pricedrop.fineprint_large}</p>
+        <!--Design Element-->
+        <div class="product__graphic-wrapper">
+          <img class="product__graphic-lines" src={lines} alt="lines" />
+        </div>
+    </div>
+  </article>
+</div>
---
+++ src/routes/price-drop/+page.svelte
@@ -0,0 +1,195 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import PriceDrop from '$lib/content/PriceDrop/PriceDrop.svelte';
+	// import PriceDrop from '$lib/content/Merch3/PriceDrop.svelte';
+	// import Merch3Drop from '$lib/content/Merch3/Merch3Drop.svelte';
+</script>
+
+<svelte:head>
+	<title>Merch: Demo of some of the ways to layout merchandising</title>
+</svelte:head>
+
+<div class="mainContent">
+   
+	<auro-header level="2" display="800">Award Sale</auro-header>
+  <auro-header level="2" display="400">Full width</auro-header>
+	<div class="container"><PriceDrop /></div>
+  
+	 <div class="grid-3col">
+		<div class="container">
+      <auro-header level="2" display="400">Side nav</auro-header>
+      <PriceDrop />
+    </div>
+		<div class="container"><auro-header level="2" display="400">Main - 3 column layout</auro-header><PriceDrop /></div>
+		<div class="container"><auro-header level="2" display="400">Anchor nav</auro-header><PriceDrop /></div>
+	</div>
+	<div class="grid-2colSideNav">
+		<div class="container"><auro-header level="2" display="400">Side nav</auro-header><PriceDrop /></div>
+		<div class="container"><auro-header level="2" display="400">Main - 2 column layout</auro-header><PriceDrop /></div>
+	</div>
+	<div class="grid-2colAnchorNav">
+		<div class="container"><auro-header level="2" display="400">Main - 2 column layout</auro-header><PriceDrop /></div>
+		<div class="container"><auro-header level="2" display="400">Anchor nav</auro-header><PriceDrop /></div>
+	</div>
+	<div class="grid-2col5050">
+		<div class="container"><auro-header level="2" display="400">50/50</auro-header><PriceDrop /></div>
+		<div class="container"><auro-header level="2" display="400">50/50</auro-header><PriceDrop /></div>
+	</div>
+
+	<hr>
+
+
+	<auro-header level="2" display="800">Flight Deals</auro-header>
+  <auro-header level="2" display="400">Full width</auro-header>
+	<div class="container"><PriceDrop /></div>
+  
+	<div class="grid-3col">
+		<div class="container">
+      <auro-header level="2" display="400">Side nav</auro-header>
+      <PriceDrop />
+    </div>
+		<div class="container"><auro-header level="2" display="400">Main - 3 column layout</auro-header><PriceDrop /></div>
+		<div class="container"><auro-header level="2" display="400">Anchor nav</auro-header><PriceDrop /></div>
+	</div>
+	<div class="grid-2colSideNav">
+		<div class="container"><auro-header level="2" display="400">Side nav</auro-header><PriceDrop /></div>
+		<div class="container"><auro-header level="2" display="400">Main - 2 column layout</auro-header><PriceDrop /></div>
+	</div>
+	<div class="grid-2colAnchorNav">
+		<div class="container"><auro-header level="2" display="400">Main - 2 column layout</auro-header><PriceDrop /></div>
+		<div class="container"><auro-header level="2" display="400">Anchor nav</auro-header><PriceDrop /></div>
+	</div>
+	<div class="grid-2col5050">
+		<div class="container"><auro-header level="2" display="400">50/50</auro-header><PriceDrop /></div>
+		<div class="container"><auro-header level="2" display="400">50/50</auro-header><PriceDrop /></div>
+	</div>
+
+	<hr>
+	<auro-header level="2" display="800">Price Drop</auro-header>
+  <auro-header level="2" display="400">Full width</auro-header>
+	<div class="container"><PriceDrop /></div>
+  
+	<!-- <div class="grid-3col">
+		<div class="container">
+      <auro-header level="2" display="400">Side nav</auro-header>
+      <Merch3Drop />
+    </div>
+		<div class="container"><auro-header level="2" display="400">Main - 3 column layout</auro-header><Merch3Drop /></div>
+		<div class="container"><auro-header level="2" display="400">Anchor nav</auro-header><Merch3Drop /></div>
+	</div>
+	<div class="grid-2colSideNav">
+		<div class="container"><auro-header level="2" display="400">Side nav</auro-header><Merch3Drop /></div>
+		<div class="container"><auro-header level="2" display="400">Main - 2 column layout</auro-header><Merch3Drop /></div>
+	</div>
+	<div class="grid-2colAnchorNav">
+		<div class="container"><auro-header level="2" display="400">Main - 2 column layout</auro-header><Merch3Drop /></div>
+		<div class="container"><auro-header level="2" display="400">Anchor nav</auro-header><Merch3Drop /></div>
+	</div>
+	<div class="grid-2col5050">
+		<div class="container"><auro-header level="2" display="400">50/50</auro-header><Merch3Drop /></div>
+		<div class="container"><auro-header level="2" display="400">50/50</auro-header><Merch3Drop /></div>
+	</div> -->
+
+	<hr>
+	
+	<h2>What's next?</h2>
+	
+	<ul>
+		<li>Create and additional 10-15 different layouts or variants</li>
+		<li>Use an API driven approach to change the words, images or colors to create an entirely new offer while using the same code.</li>
+		<li>UX copywriters can see what the copy will look like on all placements at once.</li>
+		<li>Visual designers can choose colors and images and see how they will look on all placements at once.</li>
+	</ul>
+</div>
+
+<style>
+	.grid-3col {
+		display: grid;
+		gap: 1rem;
+		margin-top: 4rem;
+	}
+  @media (min-width: 1024px) {
+    .grid-3col {
+      grid-template-columns: 280px auto 168px;
+    }
+  }
+
+	.grid-2colSideNav {
+		display: grid;
+		gap: 1rem;
+		margin-top: 4rem;
+	}
+  @media (min-width: 768px) {
+    .grid-2colSideNav {
+      grid-template-columns: 280px auto;
+    }
+  }
+	.grid-2colAnchorNav {
+		display: grid;
+		gap: 1rem;
+		margin-top: 4rem;
+	}
+  @media (min-width: 768px) {
+    .grid-2colAnchorNav {
+      grid-template-columns: auto 168px;
+    }
+  }
+	.grid-2col5050 {
+		display: grid;
+		gap: 1rem;
+		margin-top: 4rem;
+	}
+  @media (min-width: 768px) {
+    .grid-2col5050 {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+	.mainContent {
+		padding: var(--padding);
+		display: block;
+	}
+	.row {
+		margin-bottom: 3rem;
+	}
+	.container {
+		container-type: inline-size;
+	}
+	.container-xxs {
+		max-width: 140px;
+	}
+
+	.container-xs {
+		max-width: 168px;
+	}
+
+	.container-sm {
+		max-width: 360px;
+	}
+
+	.container-md {
+		max-width: 500px;
+	}
+
+	.container-lg {
+		max-width: 700px;
+	}
+
+	.container-xl {
+		max-width: 900px;
+	}
+
+	.container-xxl {
+		max-width: 1000px;
+	}
+
+	.container-xxxl {
+		max-width: 1232px;
+	}
+	hr {
+		border: .5px dashed #eee;
+		margin: 3rem 0;
+	}
+</style>
