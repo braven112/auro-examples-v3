6bc8638ab36b77a8296140202714262d7f3a2e40
Brandon Shields <braven112@gmail.com>
2023-10-25 16:56:39 -0700

    remove unused import

--- src/lib/content/Merch4/Merch4.svelte
+++ src/lib/content/Merch4/Merch4.svelte
@@ -2,7 +2,6 @@
   import "./Merch4.css";
   import card from '$lib/images/as-card.svg';
   import campaign from '../personal/Campaign_DataCC.json';
-  import flight from '../personal/Flight_Data.json';
 </script>
 <div class="merch-container">
 <article class="merch" data-variant="4">
@@ -11,13 +10,13 @@
     <!--Title-->  
     <p class="product__title-md" data-type="title-md">{campaign.titleMd}</p>  
     <!--Headlines-->  
-      <p class="product__headline-md" data-type="headline-md">{campaign.headlineMd}</p>
-      <!--CTA-->
-      <auro-hyperlink ondark class="product__cta-md" data-type="standard" type="nav" href="{campaign.ctaLinkMd}?{campaign.campaignId}">{campaign.ctaMd}</auro-hyperlink>
-      <!--Disclaimer-->
-      <p class="product__fineprint-sm fineprint">{campaign.fineprintSm}</p>
-      <p class="product__fineprint-md fineprint">{campaign.fineprintMd}</p>
-      <p class="product__fineprint-lg fineprint">{campaign.fineprintLg}</p>
+    <p class="product__headline-md" data-type="headline-md">{campaign.headlineMd}</p>
+    <!--CTA-->
+    <auro-hyperlink ondark class="product__cta-md" data-type="standard" type="nav" href="{campaign.ctaLinkMd}?{campaign.campaignId}">{campaign.ctaMd}</auro-hyperlink>
+    <!--Disclaimer-->
+    <p class="product__fineprint-sm fineprint">{campaign.fineprintSm}</p>
+    <p class="product__fineprint-md fineprint">{campaign.fineprintMd}</p>
+    <p class="product__fineprint-lg fineprint">{campaign.fineprintLg}</p>
   </div>
   <picture class="merch__img">
       <img class="product__graphic-detail" src={card} alt="card" />
