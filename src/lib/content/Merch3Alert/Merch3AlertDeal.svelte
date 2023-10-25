dca3ccfda621ae2aa946c60c5d7fbc38e77223f1
Brandon Shields <braven112@gmail.com>
2023-10-25 16:23:50 -0700

    bug fix

--- src/lib/content/Merch3Alert/Merch3AlertDeal.svelte
+++ src/lib/content/Merch3Alert/Merch3AlertDeal.svelte
@@ -1,7 +1,7 @@
 <script>
 	import lines from '$lib/images/lines.svg';
-	import campaign from '../Merch3/Campaign_Data3.json';
-	import flight from '../Merch3/Flight_Data2.json';
+	import campaign from '../personal/Campaign_Data3.json';
+	import flight from '../personal/Flight_Data2.json';
 	import './Merch3Alert.css';
 </script>
 <div class="merch-container">
